7dadf0d861ef6d4fff7dfc78d78b449c0172acf4
pokkh <erich.scheid@gmail.com>
2019-01-26 12:50:23 -0800

    revert to stable

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -123,8 +123,6 @@ import org.firstinspires.inspection.RcInspectionActivity;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import android.hardware.Camera;
-
 @SuppressWarnings("WeakerAccess")
 public class FtcRobotControllerActivity extends Activity
   {
@@ -180,56 +178,6 @@ public class FtcRobotControllerActivity extends Activity
     }
 
   }
-
-      /////////////////////////////////////////////////////////
-      // ADDED FOR CAMERA!!!
-
-      public void initPreview(final Camera camera, final OpModeCamera context, final Camera.PreviewCallback previewCallback) {
-          runOnUiThread(new Runnable() {
-              @Override
-              public void run() {
-                  context.preview = new CameraPreview(FtcRobotControllerActivity.this, camera, previewCallback);
-                  FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-                  previewLayout.addView(context.preview);
-              }
-          });
-      }
-
-      // poor coding style here.  Shouldn't have to duplicate these routines for regular and linear OpModes.
-      public void initPreviewLinear(final Camera camera, final LinearOpModeCamera context, final Camera.PreviewCallback previewCallback) {
-          runOnUiThread(new Runnable() {
-              @Override
-              public void run() {
-                  context.preview = new CameraPreview(FtcRobotControllerActivity.this, camera, previewCallback);
-                  FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-                  previewLayout.addView(context.preview);
-              }
-          });
-      }
-
-
-      public void removePreview(final OpModeCamera context) {
-          runOnUiThread(new Runnable() {
-              @Override
-              public void run() {
-                  FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-                  previewLayout.removeAllViews();
-              }
-          });
-      }
-
-      public void removePreviewLinear(final LinearOpModeCamera context) {
-          runOnUiThread(new Runnable() {
-              @Override
-              public void run() {
-                  FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-                  previewLayout.removeAllViews();
-              }
-          });
-      }
-
-      // END CAMERA ADD!!!
-      //////////////////////////////////////////////
 
   protected ServiceConnection connection = new ServiceConnection() {
     @Override
